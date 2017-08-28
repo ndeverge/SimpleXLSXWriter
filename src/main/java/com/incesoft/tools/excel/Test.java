e3e5e017556c688d6a89a895da8119c987701b7c
Nicolas Deverge <ndeverge@ekito.fr>
2017-08-28 08:31:06 +0200

    Repackaging.

--- src/main/java/com/incesoft/tools/excel/Test.java
+++ src/main/java/com/incesoft/tools/excel/Test.java
@@ -15,7 +15,7 @@ public class Test {
 		wxs.increaseRow();
 		for (int i = 0; i < 5; i++) {
 			wxs.increaseRow();
-			wxs.writeRow(new String[] { "floydd" + i }, new CellFormat[] { new CellFormat(
+			wxs.writeRow(new String[] { "value" + i }, new CellFormat[] { new CellFormat(
 					(i % 2 == 0 ? Color.PINK.getRGB() : Color.GREEN.getRGB()), -1, 0) });
 		}
 		wxs.close();
